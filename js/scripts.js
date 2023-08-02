6cc34824718457040b4166748344c8a54062058b
Adrien Denis <Neirda-95@live.fr>
2023-08-02 17:46:54 +0200

    calculatrice bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,187 @@
+// Variables globales
+
+// Eléments mémoire et écran
+
+const memoireElt = document.querySelector("#memoire");
+const ecranElt = document.querySelector("#ecran");
+
+// On stocke la valeur de l'écran "précédent"
+
+let precedent = 0;
+
+// On stocke l'affichage
+
+let affichage = "";
+
+// On stock l'opération
+
+let operation = null;
+
+// On initialise la mémoire
+
+let memoire;
+
+window.onload = () => {
+  // On écoute les cliques sur les touches
+
+  let touches = document.querySelectorAll("span");
+
+  for (let touche of touches) {
+    touche.addEventListener("click", gererTouches);
+  }
+
+  // On écoute les touches du clavier
+  document.addEventListener("keydown", gererTouches);
+
+  // Récupération de la mémoire depuis le stockage local
+  memoire = localStorage.memoire ? parseFloat(localStorage.memoire) : 0;
+  if (memoire != 0) memoireElt.style.display = "initial";
+};
+
+/**
+ * Cette fonction réagit au clic sur les touches
+ */
+
+// On definit une variable pour indiquer si un point a déjà été utilisé
+let pointUtilise = false;
+
+function gererTouches(event) {
+  let touche;
+  // On liste les touches autorisées
+
+  const listeTouches = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "+",
+    "-",
+    "*",
+    "/",
+    ".",
+    "Enter",
+    "Escape",
+  ];
+
+  // On vérifie si on a l'évenement "keydown"
+  if (event.type === "keydown") {
+    // On compare la touche appuyée aux touches autorisées
+    if (listeTouches.includes(event.key)) {
+      // On empêche l'utilisation par défaut de la touche
+      event.preventDefault();
+      // On stocke la touche choisie dans la variable "touche"
+      touche = event.key;
+    }
+  } else {
+    touche = this.innerText;
+  }
+
+  if (parseFloat(touche) >= 0 || touche === ".") {
+    // Vérification pour qu'on ne puisse pas mettre plusieurs points
+
+    if (touche === "." && pointUtilise) {
+      return;
+    }
+
+    // On met à jour la valeur d'affichage et on affiche sur l'écran
+    affichage =
+      affichage === "" ? touche.toString() : affichage + touche.toString();
+
+    // On met à jour la variable pointUtilise si un point est ajouté
+    if (touche === ".") {
+      pointUtilise = true;
+    }
+
+    ecranElt.innerText = affichage;
+  } else {
+    switch (touche) {
+      //le bouton "C" réinitialise tout
+      case "C":
+      case "Escape":
+        precedent = 0;
+        affichage = "";
+        operation = null;
+        ecranElt.innerText = 0;
+        break;
+
+      // Calculs
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        //On calcule la valeur résultat de l'étape précédente
+        precedent =
+          precedent === 0
+            ? parseFloat(affichage)
+            : calculer(precedent, parseFloat(affichage), operation);
+        // On met à jour l'écran
+        ecranElt.innerText = precedent;
+        // On stocke l'opération
+        operation = touche;
+        // On réinitialise la variable d'affichage
+        affichage = "";
+        break;
+
+      case "=":
+      case "Enter":
+        //On calcule la valeur résultat de l'étape précédente
+        precedent =
+          precedent === 0
+            ? parseFloat(affichage)
+            : calculer(precedent, parseFloat(affichage), operation);
+        // On met à jour l'écran
+        ecranElt.innerText = precedent;
+        // On stocke le résultat dans la variable d'affichage
+        affichage = precedent;
+        // On réinitialise précédent
+        precedent = 0;
+        break;
+
+      // On gère la mémoire
+      case "M+":
+        // On stocke en additionnant à la valeur déjà en mémoire
+        localStorage.memoire = localStorage.memoire
+          ? parseFloat(localStorage.memoire) + parseFloat(affichage)
+          : parseFloat(affichage);
+        // On affiche le M
+        memoireElt.style.display = "initial";
+        break;
+      case "MC":
+        // On efface la mémoire
+        localStorage.memoire = 0;
+        //On efface le M
+        memoireElt.style.display = "none";
+      case "MR":
+        // On récupère la valeur stockée
+        memoire = localStorage.memoire ? parseFloat(localStorage.memoire) : 0;
+        affichage = memoire;
+        ecranElt.innerText = memoire;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+/**
+ * Effectue le calcul
+ * @param {number} nb1
+ * @param {number} nb2
+ * @param {string} operation
+ * @returns number
+ */
+function calculer(nb1, nb2, operation) {
+  nb1 = parseFloat(nb1);
+  nb2 = parseFloat(nb2);
+
+  if (operation === "+") return nb1 + nb2;
+  if (operation === "-") return nb1 - nb2;
+  if (operation === "*") return nb1 * nb2;
+  if (operation === "/") return nb1 / nb2;
+}
